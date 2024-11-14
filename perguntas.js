abf0d880978a61d28652914a5d82d40ce179ae45
samukinhas <141034867+samukinhas@users.noreply.github.com>
2024-11-14 07:36:07 -0300

    14/11

---
+++ perguntas.js
@@ -0,0 +1,5 @@
+criaCartao (
+    'Cientistas',
+    'Qual foi o físico teório que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica quântica?',
+    'Albert Einstein'
+)
