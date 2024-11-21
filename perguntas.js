dabad11d18860ec3b85e8c308d2a9c4851b1bec4
samukinhas <141034867+samukinhas@users.noreply.github.com>
2024-11-21 07:37:10 -0300

    tasalvo

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,15 @@ criaCartao (
     'Qual foi o físico teório que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica quântica?',
     'Albert Einstein'
 )
+
+criaCartao (
+    'Cientistas',
+    'Qual cientista que foi um nome muito importante na área da eletricidade, responsável por desenvolver a corrente alternada e várias outras invenções ao longo de sua vida?',
+    'Nikola Tesla'
+)
+
+criaCartao (
+    'Cientistas',
+    'Qual foi o matemático, bibliotecário, astrônomo da Grécia Antiga, conhecido por calcular a circunferência da Terra?',
+    'Eratóstenes'
+)
